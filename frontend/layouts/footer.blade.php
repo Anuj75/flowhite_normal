62e090585916555a5e98f833db56d0d367ba86aa
anuj <anuj75800@gmail.com>
2022-02-03 19:43:37 +0530

    changes update

---
+++ frontend/layouts/footer.blade.php
@@ -0,0 +1,139 @@
+  <!-- ======= Footer ======= -->
+  <footer>
+    <div class="footer-area">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <div class="footer-content">
+              <div class="footer-head">
+              
+                <div class="footer-logo">
+               
+                <img class="logo-img" src="{{url('frontend/assets/img/about/logo final.png')}}" width="130" height="130" ALT="align box" ALIGN=CENTER>
+             
+                </div>
+                            
+                <p>Flowhite smart solution Appreciate a the differing requirements of  Electronics Products. Flowhite has become a part of our everyday life with providing smart technology. we worked globally by covering private and public sectors projects and requirements of lighting products  and installation services.</p>
+                <div class="footer-icons">
+                  <ul>
+                    <li>
+                      <a href="#"><i class="bi bi-facebook"></i></a>
+                    </li>
+                    <li>
+                      <a href="#"><i class="bi bi-twitter"></i></a>
+                    </li>
+                    <li>
+                      <a href="#"><i class="bi bi-instagram"></i></a>
+                    </li>
+                    <li>
+                      <a href="#"><i class="bi bi-linkedin"></i></a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- end single footer -->
+          <div class="col-md-4">
+            <div class="footer-content">
+              <div class="footer-head">
+                <h4>information</h4>
+                <p>
+                  The best of the best is combined in flowhite smart solution .
+                </p>
+                <div class="footer-contacts">
+                  <p><span>Tel:</span> 7017084173</p>
+                  <p><span>Email:</span> Info@flowhite.in</p>
+                  <p><span>Working Hours:</span> 9am-6pm</p>
+                  <p><span>Regional office:</span> 03, Saraswati Nagar, Awadhpuri Road. Agra</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- end single footer -->
+          <div class="col-md-4">
+            <div class="footer-content">
+              <div class="footer-head">
+                <h4>facebook</h4>
+                <div class="flicker-img">
+                  <a href="#"><img src="{{url('frontend/assets/img/portfolio/1.jpg')}}" alt=""></a>
+                  <a href="#"><img src="{{url('frontend/assets/img/portfolio/2.jpg')}}" alt=""></a>
+                  <a href="#"><img src="{{url('frontend/assets/img/portfolio/3.jpg')}}" alt=""></a>
+                  <a href="#"><img src="{{url('frontend/assets/img/portfolio/4.jpg')}}" alt=""></a>
+                  <a href="#"><img src="{{url('frontend/assets/img/portfolio/5.jpg')}}" alt=""></a>
+                  <a href="#"><img src="{{url('frontend/assets/img/portfolio/6.jpg')}}" alt=""></a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="footer-area-bottom">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 col-sm-12 col-xs-12">
+            <div class="copyright text-center">
+              <p>
+                &copy; Copyright <strong>Flowhite.in</strong>. All Rights Reserved
+              </p>
+            </div>
+            <div class="credits">
+              <!--
+              All the links in the footer should remain intact.
+              You can delete the links only if you purchased the pro version.
+              Licensing information: https://bootstrapmade.com/license/
+              Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=eBusiness
+            -->
+              Designed by <a href="">Cybron Smart Solution</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer><!-- End  Footer -->
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="{{url('frontend/assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+  <script src="{{url('frontend/assets/vendor/glightbox/js/glightbox.min.js')}}"></script>
+  <script src="{{url('frontend/assets/vendor/isotope-layout/isotope.pkgd.min.js')}}"></script>
+  <script src="{{url('frontend/assets/vendor/swiper/swiper-bundle.min.js')}}"></script>
+  <script src="{{url('frontend/assets/vendor/php-email-form/validate.js')}}"></script>
+
+  <!-- Template Main JS File -->
+  <script src="{{url('frontend/assets/js/main.js')}}"></script>
+
+  <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+  <script>
+    AOS.init(
+      {offset: 200,
+        duration:1500,
+        easing: 'ease in ', }
+    );
+
+    function change_image(image){
+
+var container = document.getElementById("main-image");
+
+container.src = image.src;
+}
+
+
+
+document.addEventListener("DOMContentLoaded", function(event) {
+
+
+
+
+
+
+
+});
+  </script>
+
+</body>
+
+</html>
---
+++ frontend/layouts/header.blade.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Flowhite</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="{{url('frontend/assets/img/favicon.png')}}" rel="icon">
+  <link href="{{url('frontend/assets/img/apple-touch-icon.png')}}" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,400i,600,700|Raleway:300,400,400i,500,500i,700,800,900" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="{{url('frontend/assets/vendor/animate.css/animate.min.css')}}" rel="stylesheet">
+  <link href="{{url('frontend/assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+  <link href="{{url('frontend/assets/vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
+  <link href="{{url('frontend/assets/vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
+  <link href="{{url('frontend/assets/vendor/glightbox/css/glightbox.min.css')}}" rel="stylesheet">
+  <link href="{{url('frontend/assets/vendor/swiper/swiper-bundle.min.css')}}" rel="stylesheet">
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+  <!-- Template Main CSS File -->
+  <link href="{{url('frontend/assets/css/style.css')}}" rel="stylesheet">
+  <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+</head>
+
+<body>
+
+
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top d-flex align-items-center">
+    
+    <div class="container d-flex justify-content-between">
+
+      <div class="logo">
+        
+        <img class="logo-img" src="{{url('frontend/assets/img/about/logo final.png')}}" width="130" height="130" ALT="align box" ALIGN=CENTER>
+       
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      </div>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link" id="crew" href="{{url('/')}}">Home</a></li>
+          <li class="dropdown"><a id="crew"href="#"><span>Products</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              
+              <li class="dropdown"><a href="#"><span>Indoor Lights</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="/led-pannel">LED Pannel lights </a></li>
+                  <li><a href="/led-junction">LED Junction Lights</a></li>
+                  <li><a href="/led-striker">LED Striker lights</a></li>
+                  <li><a href="/led-tubelight">Down light</a></li>
+                  <li><a href="/down-light">LED Tubelights</a></li>
+                  <li><a href="/led-bulb">LED Bulb</a></li>
+                </ul>
+                <li class="dropdown"><a href="#"><span>Outdoor Lights</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="/led-flood">LED Flood Lights</a></li>
+                  <li><a href="/led-street">LED Street Lights</a></li>
+                  <li><a href="/led-wall">LED Wall lights</a></li>
+                  <li><a href="/led-haybay">LED Haybay Lights</a></li>
+                  
+                </ul>
+              </li>
+              <li><a href="#">Decoratives Lights</a></li>
+              <li><a href="#">Switch & MCB</a></li>
+              <li><a href="#">Junction Box</a></li>
+              <li><a href="#">Exhust Fan</a></li>
+              <li><a href="#">Condult Pipe</a></li>
+          
+            </ul>
+          </li>
+          <li><a class="nav-link scrollto"id="crew" href="#">Services</a></li>
+          <li><a class="nav-link scrollto" id="crew"href="#portfolio">Projects</a></li>
+          <li><a class="nav-link scrollto" id="crew"href="{{url('/about')}}">About</a></li>
+          <li><a id="crew"href="{{url('/blog')}}">Blog</a></li>
+         
+
+
+          <li><a class="nav-link scrollto" id="crew"href="{{url('/contact')}}">Contact</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
